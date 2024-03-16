53af3394a31c4ba0b0be5787ca48c60ae3eb201a
chriscansler <cansler7@gmail.com>
2024-03-15 22:31:41 -0400

    committing for backup

---
+++ AsynchronousRandomWords/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+string? word = null;
+
+do {
+	Console.Write("Enter a short word: ");
+	word = Console.ReadLine();
+} while(word == null);
+
+DateTime time1 = DateTime.Now;
+int attempts = await RandomlyRecreateAsync(word);
+Console.WriteLine(attempts);
+TimeSpan elapsedTime = DateTime.Now - time1;
+Console.WriteLine($"Operation took {elapsedTime}");
+
+int RandomlyRecreate(string? word) {
+	if (word == null) return 0;
+	int attempts = 0;
+	StringBuilder sb;
+	Random random = new Random();
+
+	do {
+		sb = new StringBuilder();
+		for (int i = 0; i < word.Length; i++) {
+			sb.Append((char)('a' + random.Next(26)));
+		}
+		attempts += 1;
+	} while(sb.ToString() != word);
+
+	return attempts;
+
+}
+
+Task<int> RandomlyRecreateAsync(string word) {
+	return Task.Run(() => {
+		return RandomlyRecreate(word);
+	});
+}
---
+++ ManyRandomWords/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+string? word = null;
+while (word != "quit") {
+	do {
+		Console.WriteLine("Enter a short word (\"quit\" to quit): ");
+		word = Console.ReadLine();
+	} while (word == null);
+
+	if (word != "quit") {
+		HandleWord(word);
+	}
+}
+
+int RandomlyRecreate(string? word) {
+	if (word == null) return 0;
+	int attempts = 0;
+	StringBuilder sb;
+	Random random = new Random();
+
+	do {
+		sb = new StringBuilder();
+		for (int i = 0; i < word.Length; i++) {
+			sb.Append((char)('a' + random.Next(26)));
+		}
+		attempts += 1;
+	} while (sb.ToString() != word);
+
+	return attempts;
+
+}
+
+async Task HandleWord(string word) {
+	DateTime time1 = DateTime.Now;
+	int attempts = await RandomlyRecreateAsync(word);
+	TimeSpan elapsedTime = DateTime.Now - time1;
+	ConsoleColor oldColor = Console.ForegroundColor;
+	Console.ForegroundColor = ConsoleColor.White;
+	Console.WriteLine($"Operation for \"{word}\" took {attempts} attempts and {elapsedTime}");
+	Console.ForegroundColor = oldColor;
+}
+
+Task<int> RandomlyRecreateAsync(string word) {
+	return Task.Run(() => {
+		return RandomlyRecreate(word);
+	});
+}
